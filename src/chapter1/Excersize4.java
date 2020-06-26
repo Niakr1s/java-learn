d7ceac0053c6e7ebb7d4bcbf354f2f94cbec3c23
Niakr1s <pavel2188@gmail.com>
2020-06-26 16:55:24 +0300

    resolved exercise 1.4

---
+++ src/chapter1/Excersize4.java
@@ -0,0 +1,7 @@
+package chapter1;
+
+public class Excersize4 {
+    public static void main(String[] args) {
+        System.out.println(Math.nextUp(0) + ", " + Math.nextDown(Double.MAX_VALUE));
+    }
+}
