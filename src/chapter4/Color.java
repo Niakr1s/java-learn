a5018083ad005d585ac1ec082f61bd76bb210c78
Niakr1s <pavel2188@gmail.com>
2020-06-28 12:50:59 +0300

    solved Chapter4.Exercise7

---
+++ src/chapter4/Color.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+public enum Color {
+    BLACK, RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+
+    static Color getRed() {
+        return RED;
+    }
+
+    static Color getGreen() {
+        return GREEN;
+    }
+
+    static Color getBlue() {
+        return BLUE;
+    }
+}
